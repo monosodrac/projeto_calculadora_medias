f5c87060fb5b8ed5cc895e5fb6978c763a0c1383
Mono <rickyblesstf@gmail.com>
2024-04-16 20:11:59 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,63 @@
+const form = document.querySelector('#form-atividade');
+const imgAprovado = '<img src="images/aprovado.png" alt="Emoji festejando">';
+const imgReprovado = '<img src="images/reprovado.png" alt="Emoji decepcionado">';
+const atividades = [];
+const notas = [];
+const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
+const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
+const notaMinima = parseFloat(prompt('Digita a nota miníma:'));
+
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+    atualizaMediaFinal();
+})
+
+function adicionaLinha() {
+    const nomeAtividade = document.getElementById('nome-atividade');
+    const notaAtividade = document.getElementById('nota-atividade');
+
+    if (atividades.includes(nomeAtividade.value)) {
+        alert(`A atividade ${nomeAtividade.value} já foi inserida`);
+    } else {
+        atividades.push(nomeAtividade.value);
+        notas.push(parseFloat(notaAtividade.value));
+
+        let linha = '<tr>';
+        linha += `<td>${nomeAtividade.value}</td>`;
+        linha += `<td>${notaAtividade.value}</td>`;
+        linha += `<td>${notaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`;
+        linha += '</tr>';
+        
+        linhas += linha;
+    }
+
+    nomeAtividade.value = '';
+    notaAtividade.value = '';
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
+
+function atualizaMediaFinal() {
+    const mediaFinal = calculaMediaFinal();
+
+    document.getElementById('media-final-valor').innerHTML = mediaFinal;
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
+}
+
+function calculaMediaFinal() {
+    let somaDasNotas = 0;
+
+    for (let i = 0; i < notas.length; i++) {
+        somaDasNotas += notas[i];
+    }
+
+    return somaDasNotas / notas.length;
+}
